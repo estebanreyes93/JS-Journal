4dea8ed70c51487acd6e2cbc7e4350c96a448862
estebanreyes93 <44317982+estebanreyes93@users.noreply.github.com>
2024-05-07 14:45:33 -0400

    Booleans, strings and numbers
    Set of tests using Booleans, strings, numbers, conditionals

--- index.js
+++ index.js
@@ -1,66 +1,38 @@
-//testing console.log
-console.log(`Oye, brayan!`);
-console.log(`el brayan e duro`);
-
-//testing window.alert
-window.alert(`hola, brayan`);
-window.alert(`el brayan e dios`);
-window.alert(`brayan e vacano`);
-
-<<<<<<< Updated upstream
-//testing window.prompt
-window.prompt(`a u ok?`, `yes'nth`);
-window.prompt(`a u ok?`, `no'th`);
-=======
-//This is an example of a variable declaration.
-
-/*
-let x;
-x = 123;
-*/
-
-//This is an example of a variable assignment and declaration.
-
-/*
-let x = 100;
-console.log(x);
-*/
-
 //STRINGS
-// let name = "John";
-// let age = 31;
-// let price = 9.99;
-// let gpa = 3.5;
+let name = "John";
+let age = 31;
+let price = 9.99;
+let gpa = 3.5;
 
 //typeof is for data determination console.log(typeof age);
 
 //NUMBERS
-// console.log(typeof age);
-// console.log(`You are ${age} years old.`);
-// console.log(typeof price);
-// console.log(`The price is $${price}.`);
-// console.log(typeof gpa);
-// console.log(`Your gpa is ${gpa}.`);
-// console.log(typeof name);
-// console.log(`Your Name is ${name}.`);
+console.log(typeof age);
+console.log(`You are ${age} years old.`);
+console.log(typeof price);
+console.log(`The price is $${price}.`);
+console.log(typeof gpa);
+console.log(`Your gpa is ${gpa}.`);
+console.log(typeof name);
+console.log(`Your Name is ${name}.`);
 
 //STRINGS
-// let firstName = "Esteban";
-// let favoriteFood = "Tacos";
-// let email = "esteban@my.com";
+let firstName = "Esteban";
+let favoriteFood = "Tacos";
+let email = "esteban@my.com";
 
-// console.log(typeof firstName);
-// console.log(`Your Name is ${firstName}
-//             and your favorite food
-//             is ${favoriteFood}, also your mail is
-//             ${email}`);
+console.log(typeof firstName);
+console.log(`Your Name is ${firstName}
+            and your favorite food
+            is ${favoriteFood}, also your mail is
+            ${email}`);
 
 //BOOLEANS Commonly used for true or false and if elses
 
-// let online = false;
-// let forSale = true;
-// console.log(`Is this car for sale? ${forSale}?`);
-// console.log(`Brayan is online: ${online}`);
+let online = false;
+let forSale = true;
+console.log(`Is this car for sale? ${forSale}?`);
+console.log(`Brayan is online: ${online}`);
 
 let fullName = "Esteban";
 let age = 32;
@@ -69,4 +41,3 @@ let student = false;
 document.getElementById("p1").textContent = fullName;
 document.getElementById("p2").textContent = age;
 document.getElementById("p3").textContent = student;
->>>>>>> Stashed changes
